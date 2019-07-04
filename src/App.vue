aa6cbb31e74fb058bfc71376380acf0156271c6e
GotchaGuy <aljosa100@gmail.com>
2019-07-04 14:32:27 +0200

    legitness

---
+++ src/App.vue
@@ -0,0 +1,81 @@
+<template>
+  <div id="app">
+      <section>
+     <h1>20:19</h1>
+      <p>26th May 2021</p>
+      <Notifications v-bind:info="this.allNotifs" />
+      </section>
+  </div>
+</template>
+
+<script>
+import Notifications from "./components/Notifs.vue"
+import axios from "axios"
+
+export default {
+  name: 'app',
+  data(){
+        return {
+            notifList: "https://api.myjson.com/bins/19wyhe",
+            allNotifs: [],
+
+        }
+    },
+    mounted() {
+        axios.get(this.notifList)
+        .then ( res => {
+
+          this.allNotifs = res.data.sort(function(a, b){
+            return b.timestamp - a.timestamp;
+          })
+  
+        })
+    console.log(this.allNotifs);
+    
+    },
+    methods: {
+      
+    },
+  components: {
+    Notifications
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  /* margin-top: 60px; */
+}
+body {
+            margin: 0;
+            padding: 0;
+            
+        }
+        section {
+            width: 600px;
+            min-height: 1000px;
+            margin: 0 auto;
+            background-image: url("https://cdn.pixabay.com/photo/2016/02/19/11/00/palm-tree-1209536_960_720.jpg"); 
+            background-size: cover;
+            text-align: center;
+        }
+        h1 {
+            display: inline-block;
+            font-size: 120px;
+            font-weight: bold;
+            margin: 0;
+            color: white;
+        }
+        p {
+            font-size: 25px;
+            font-weight: bold;
+            margin-top: 0;
+            
+        }
+        
+</style>
---
+++ src/components/Notifs.vue
@@ -0,0 +1,67 @@
+
+<template>
+    <section>
+            <div class="notification" v-bind:class="{'seen':notif.seen}" v-for="(notif, index) in info" v-bind:key="index" >
+
+                <div>
+                    <img :src="notif.icon" alt="icon">
+                </div>
+                <div class="text">
+                    <p >{{notif.message}}</p>
+                </div>
+            </div>
+        </section>
+</template>
+
+   <script>
+export default {
+    name: "Notifications",
+        props: {
+        info: Array
+    },
+
+
+}
+</script>
+
+<style lang="scss">
+       
+        div.notification {
+            width: 560px;
+            height: 80px;
+            color: rgb(37, 46, 54);
+            background-color: rgb(82, 82, 82);
+            box-shadow: 1px solid black;
+            margin-bottom: 10px;
+            margin: 0 auto 10px auto;
+            
+            div {
+                height: 80px;
+                width: 80px;
+                float: left;
+                img {
+                    height: 100%;
+                    width: auto;
+                }
+            }
+            div.text {
+                    height: 80px;
+                    width: 480px;
+                    float: left;
+                p {
+                    height: 30px;
+                    font-size: 16px;
+                    line-height: 30px;
+                    text-align: left;
+                    color: rgb(0, 0, 0);
+                    background-color: rgb(255, 184, 51);
+                }
+
+            }
+        }
+        .seen {
+            opacity: 0.5;
+        }
+    </style>
+
+ 
